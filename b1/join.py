ab14839301678342d1211f14d6daa132762b4b95
Brendan O'Connor <brenocon@gmail.com>
2024-10-13 20:06:08 -0400

    scripts for per-event table in gdoc sept21

---
+++ b1/join.py
@@ -0,0 +1,28 @@
+# Joins EMDAT metadata along with our per-event news coverage data.
+# output from this script does NOT include EMDAT instances with 0 news coverage!!
+
+import json,glob
+
+kb = json.load(open("emdat-disaster-instance-info.json"))
+efiles = glob.glob("passing-phase-2-en-var1/*.jsonl")
+for ff in efiles:
+    eid = ff.split("/")[-1].replace("-finalout.jsonl","")
+    # print(eid in kb, eid)
+    # if eid not in kb: continue
+
+    for dd in (json.loads(line) for line in open(ff)):
+        # del dd['text']
+
+        dd['eid']=eid
+        # dd['e_date_orig'] = "orig" + "-".join(eid.split("-")[-3:])
+        dd['e_date'] = "-".join(eid.split("-")[-3:]).replace("-xx","-01")
+        dd['e_type']=eid.split("-")[0]
+
+        for k in ['total affected','num affected','total deaths', 'num homeless', 'num injured']:
+            val = kb[eid][k] if eid in kb else ""
+            val = "" if val=="-999" else val
+            k2 = k.replace(" ","_").replace("num","n").replace("affected","aff").replace("total","tot").replace("homeless","hl").replace("injured","inj").replace("deaths","de")
+            dd[k2] = val
+
+        print(json.dumps(dd))
+        
